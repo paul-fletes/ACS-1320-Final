fc5df979474d10ba26eef7d6b5d537eff4f5ae7e
Paul Fletes <paul.fletes@gmail.com>
2023-03-01 15:53:35 -0600

    update band styles and style counter

--- src/components/Band/Band.js
+++ src/components/Band/Band.js
@@ -3,22 +3,28 @@ import './Band.css';
 import Counter from "../Counter/Counter";
 
 const Band = (props) => {
-  const { Band_name, origin, fans, formed, split, styles } = props
+  const { Band_name, origin, fans, formed, split, styles } = props;
+  let numFans = parseInt(fans) * 1000;
+  numFans = numFans.toLocaleString('en');
+  const bandStyle = styles.split(',').map((item, index) => {
+    return <li key={index}>{item}</li>
+  });
+
   return (
     <div className="Band">
       <h1>{Band_name}</h1>
       <div className="bandInfo">
         <p><strong>Origin:</strong> {origin}</p>
-        <p><strong>Fans:</strong> {fans * 1000}</p>
+        <p><strong>Fans:</strong> {numFans}</p>
         <p><strong>Formed:</strong> {formed}</p>
         <p><strong>Split:</strong> {split}</p>
       </div>
       <p>{styles}</p>
       <div className="styleList">
-        <p>insert bullet style list here</p>
+        <ul className="bandStyle">{bandStyle}</ul>
       </div>
       <div className="likeCounter">
-        <Counter />
+        {split === '-' ? <Counter /> : null}
       </div>
     </div>
   )
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { useState } from "react";
+import './Counter.css';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="Counter">
-      <button onClick={() => setCount(
-        count + 1
-      )}>👍🏼</button>
-      <h2>{count}</h2>
-      <button onClick={() => setCount(
-        count - 1
-      )}>👎🏼</button>
+      <button onClick={() => setCount(count - 1)}>👎🏼</button>
+      <h2 className="number">{count}</h2>
+      <button onClick={() => setCount(count + 1)}>👍🏼</button>
     </div>
   )
 };
